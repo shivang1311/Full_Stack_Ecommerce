115ec811bd24bd5e33be4a24382f19bb0754d66f
Shivang agrawal <119686924+shivang1311@users.noreply.github.com>
2024-11-18 01:47:42 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import men_banner from "./Components/Assets/banner_mens.png";
 import kid_banner from "./Components/Assets/banner_kids.png";
 import LoginSignup from "./Pages/LoginSignup";
 
-export const backend_url = 'https://full-stack-ecommerce-backend-v4oa.onrender.com';
+export const backend_url = 'https://full-stack-ecommerce-backendd.onrender.com/';
 export const currency = '₹';
 
 function App() {
