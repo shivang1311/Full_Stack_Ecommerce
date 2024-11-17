2f5a2228ab8e9ae6eb602ad3a87bc8a99c141625
Shivang agrawal <119686924+shivang1311@users.noreply.github.com>
2024-11-18 01:05:40 +0530

    App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,7 +3,7 @@ import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Admin from "./Pages/Admin";
 
-export const backend_url = 'http://localhost:4000';
+export const backend_url = 'https://full-stack-ecommerce-backend-v4oa.onrender.com';
 export const currency = '₹';
 
 function App() {
